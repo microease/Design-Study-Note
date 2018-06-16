24a2996c9ee98e4f5ede0410c0fb65b7ffd0529e
胡小易 <microease@163.com>
2018-06-16 23:20:30 +0800

    add

---
+++ 黑马/03Javascript 基础/01/006.js
@@ -0,0 +1,16 @@
+// document.write("<a href='http://www.baidu.com'>百度</a>");
+// document.write("<br>⭐️");
+// for (var i=0;i<10;i++){
+//     document.write("<br>⭐️");
+// }
+// document.write("<br>⭐️");
+//
+//三角形的五角星
+for(var i=1; i<=5;i++){
+    //控制行数
+    for(var j=1 ;j<=i;j++){
+        //控制星星个数
+        document.write("⭐");
+    }
+    document.write("<br>️");
+}
---
+++ 黑马/03Javascript 基础/01/007.js
@@ -0,0 +1,7 @@
+//乘法口诀表
+for(i=1;i<=9;i++){
+    for(j=1;j<=i;j++){
+        document.write(j+"*"+i+"="+i*j+"&nbsp;");
+    }
+    document.write("<br>")
+}
