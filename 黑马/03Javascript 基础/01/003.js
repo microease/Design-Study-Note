c598b07cabf01e1e1338a5255e741f35a3ba2111
胡小易 <microease@163.com>
2018-06-16 20:31:43 +0800

    add

---
+++ 黑马/03Javascript 基础/01/003.js
@@ -0,0 +1,19 @@
+//求100以内的所有3的倍数的和
+// var sum = 0;
+// var i = 1;
+
+while(i<=100){
+    if(i%3==0){
+        sum += i;
+    }
+    i+=1;
+}
+console.log(sum);
+
+// //do while
+// var i = 1;
+// var sum = 0;
+
+// do {
+    
+// } while (condition);
