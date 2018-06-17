978c7202856a4902abf86848bef489caa9c62417
胡小易 <microease@163.com>
2018-06-17 13:32:28 +0800

    add

--- 黑马/03Javascript 基础/02/001.js
+++ 黑马/03Javascript 基础/02/001.js
@@ -1,8 +1,40 @@
 //找到100到200之间可以被7整除的第一个数字
 
-for(var num=100;num<=200;num++){
-    if(num%7==0){
-        document.write(num);
-        break;
+// for(var num=100;num<=200;num++){
+//     if(num%7==0){
+//         document.write(num);
+//         break;
+//     }
+// }
+
+//求100到200之间所有奇数的和，用continue
+//答案是7500
+//此处没有用到continue
+// var sum=0;
+// for(var num=100;num<=200;num++){
+//     if(num%2!=0){
+//     sum += num;
+//     }
+//
+// }document.write(sum+"<br>");
+
+//此处用continue
+// var sum=0;
+// for(var num=100;num<=200;num++){
+//     if(num%2==0){
+//         continue;
+//     }
+//     sum+=num;
+//     document.write(sum+"<br>");
+//
+// }
+
+//求整数1-100的累计值，但要求跳过所有个位数为3的数
+var sum = 0;
+for(i=1;i<=100;i++){
+    if(i%10==3){
+        continue;
     }
-}
+    sum += i;
+
+}document.write(sum+"<br>");
---
+++ 黑马/03Javascript 基础/02/002.js
@@ -0,0 +1,9 @@
+//构造函数定义数组
+var arr1 = new Array(1,2,3);
+document.write(arr1[0]);
+
+
+// //字面量方式构造数组
+// var arr1 = [1,2,3,4,5];
+// document.write(arr1[2]);
+//
