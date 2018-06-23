0a5a2e6a921a2fb0f95703c5a4736bec661ac04d
胡小易 <microease@163.com>
2018-06-23 19:47:01 +0800

    add

---
+++ 黑马/03Javascript 基础/02/009.js
@@ -0,0 +1,34 @@
+// 给定一个字符串如：“abaasdffggghhjjkkgfddsssss3444343”问题如下： 
+// 1、 字符串的长度
+// 2、 取出指定位置的字符，如：0,3,5,9等
+// 3、 查找指定字符是否在以上字符串中存在，如：i，c ，b等
+// 4、 替换指定的字符，如：g替换为22,ss替换为b等操作方法
+// 5、 截取指定开始位置到结束位置的字符串，如：取得1-5的字符串
+// 6、 找出以上字符串中出现次数最多的字符和出现的次数
+// 7、 遍历字符串，并将遍历出的字符两头添加符号“@”输出至当前的文档页面。
+
+var zuoye = "abaasdffggghhjjkkGfddssSSs3444343";
+// 1、 字符串的长度
+console.log(zuoye.length);
+// 2、 取出指定位置的字符，如：0,3,5,9等
+console.log(zuoye.charAt(0));
+// 3、 查找指定字符是否在以上字符串中存在，如：i，c ，b等
+console.log(zuoye.search("s"));
+// 4、 替换指定的字符，如：g替换为22,ss替换为b等操作方法
+// do {
+//     zuoye = zuoye.replace("g","22");
+//     zuoye = zuoye.replace("ss","b");
+// }while (zuoye.indexOf("g")>-1);
+// console.log(zuoye);
+// 5、 截取指定开始位置到结束位置的字符串，如：取得1-5的字符串
+// zuoye = zuoye.slice(1,6);
+// console.log(zuoye);
+// 6、 找出以上字符串中出现次数最多的字符和出现的次数
+zuoye = zuoye.toLocaleLowerCase();
+
+
+console.log(zuoye);
+
+
+
+// 7、 遍历字符串，并将遍历出的字符两头添加符号“@”输出至当前的文档页面。
