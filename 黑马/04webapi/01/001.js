df6a9e20e628b016e564abc0fa866f461e013e97
胡小易 <microease@163.com>
2018-06-24 14:04:44 +0800

    add

--- 黑马/04webapi/01/001.js
+++ 黑马/04webapi/01/001.js
@@ -1,3 +1,3 @@
-document.getElementById("btn").onclick=function () {
-    alert("hello,huyankai")
-}
+document.getElementById("btn").onclick = function () {
+    alert("hello,huyankai");
+};
---
+++ 黑马/04webapi/01/002.js
@@ -0,0 +1,9 @@
+//点击按钮显示图片
+document.getElementById("btn").onclick=function () {
+    //document.getElementById("img").src="images/liuyan.jpg";
+    //document.getElementById("img").height="500";
+    //点击按钮修改p标签显示的内容
+    document.getElementById("p").innerText="hello,huyankai";
+    document.getElementById("a").innerText="我在写代码";
+    document.getElementById("a").href="http://www.wzxdm.com/";
+}
