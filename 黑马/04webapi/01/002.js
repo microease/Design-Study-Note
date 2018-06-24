902d15797e56052df48dc29bbf85e3b490d409e5
胡小易 <microease@163.com>
2018-06-24 15:47:51 +0800

    add

--- 黑马/04webapi/01/002.js
+++ 黑马/04webapi/01/002.js
@@ -3,7 +3,18 @@ document.getElementById("btn").onclick=function () {
     //document.getElementById("img").src="images/liuyan.jpg";
     //document.getElementById("img").height="500";
     //点击按钮修改p标签显示的内容
-    document.getElementById("p").innerText="hello,huyankai";
-    document.getElementById("a").innerText="我在写代码";
-    document.getElementById("a").href="http://www.wzxdm.com/";
+    // document.getElementById("p").innerText="hello,huyankai";
+    // document.getElementById("a").innerText="我在写代码";
+    // document.getElementById("a").href="http://www.wzxdm.com/";
+    //点击按钮修改多个p标签的内容
+    // var test = document.getElementsByTagName("p");
+    // for(var i =0;i<test.length;i++){
+    //     test[i].innerText="hello,huyankai";
+    //     // console.log(test[i]);
+    // }
+    document.getElementById("img").src="images/boduo.jpg";
+    document.getElementById("img").title="波多野结衣";
+    document.getElementById("img").alt="波多野结衣";
+
+
 }
