616c89379be462c567aa05271a19f71a8da92966
microease <microease@163.com>
2018-08-09 01:02:10 +0800

    add

---
+++ 001黑马/09阿里百秀/baixiu/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','baixiu');
+define('DB_NAME','baixiu');
+define('DB_PASS','CDykH2mfXsj3xsSK');
+
+?>
---
+++ 001黑马/09阿里百秀/baixiu/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huxiaoyi
+ * Date: 2018/8/9
+ * Time: 上午12:37
+ */
+require_once 'config.php';
+?>
+<h1>前台页面</h1>
+
+<p><?php echo DB_USER ?></p>
