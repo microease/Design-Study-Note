b8183ae792f23b6391c0acc064d99e61cf29db8e
microease <microease@163.com>
2018-08-20 18:42:32 +0800

    add

---
+++ 001黑马/10HTML5&CSS3/01/001fullpage/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.container').fullpage({
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered:false,
+        navigation:true,
+    });
+});
